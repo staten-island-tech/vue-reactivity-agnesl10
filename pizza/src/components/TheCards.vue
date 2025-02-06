7b9d2820d99bb8ec756f755b91ac80e97d174a9f
agnesl10 <158211772+agnesl10@users.noreply.github.com>
2025-02-05 20:12:33 -0500

    werwer

--- pizza/src/components/TheCards.vue
+++ pizza/src/components/TheCards.vue
@@ -8,7 +8,7 @@
 const toppings = [
   { name: 'basil', icon: 'pizza\public\basilicon.png',image:'pizza\public\basil.png', topic: 'herb' },
   { name: 'bellpepper', icon: 'pizza\public\bellpeppericon.png',image:'pizza\public\bellpepper.png', topic: 'vegetable' },
-  { name: 'cheese', icon: 'pizza\public\cheeseicon.png',image:'', topic: 'dairy' },
+  { name: 'cheese', icon: 'pizza\public\cheeseicon.png',image:'pizza\public\cheese.png', topic: 'dairy' },
   { name: 'sauce', icon: 'pizza\public\sauceicon.png',image:'pizza\public\sauce.png', topic: 'base' },
   { name: 'eggplant', icon: 'pizza\public\eggplanticon.png',image:'pizza\public\eggplant.png', topic: 'vegetable' },
   { name: 'mushroom', icon: 'pizza\public\mushroomicon.png',image:'pizza\public\mushrrom.png', topic: 'fungus' },
